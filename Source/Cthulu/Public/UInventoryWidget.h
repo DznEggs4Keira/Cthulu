a0f27c20e33765b41c83d5b80673b8999cc1c546
DznEggs4Keira <purple.fire1996.za@gmail.com>
2026-03-16 23:22:54 +0100

    Add a base class for the inventory widget

---
+++ Source/Cthulu/Public/UInventoryWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UInventoryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CTHULU_API UUInventoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
